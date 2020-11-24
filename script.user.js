f75ceb672f7328701df664d2489514e182e002db
inahym-tech <68362849+inahym-tech@users.noreply.github.com>
2020-11-25 03:07:42 +0900

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -4,9 +4,9 @@
 // @include     https://example.com/
 // @updateURL   https://github.com/inahym-tech/TestScript/raw/main/script.user.js
 // @supportURL  https://github.com/inahym-tech/TestScript/raw/main/script.user.js
-// @version     1.0
+// @version     1.1
 // ==/UserScript==
 
 (function(){
-  alert("v1.0");
+  alert("v1.1");
 })();
