a110c33be79011ca9e606315d8242f328d4034cd
inahym-tech <68362849+inahym-tech@users.noreply.github.com>
2020-11-25 03:06:28 +0900

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        script
+// @namespace   script
+// @include     https://example.com/
+// @updateURL   https://github.com/inahym-tech/TestScript/
+// @supportURL  https://github.com/inahym-tech/TestScript/
+// @version     1.0
+// ==/UserScript==
+
+(function(){
+  alert("v1.0");
+})();
