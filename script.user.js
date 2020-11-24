626b31259f412e4e8d5cc9e50ae4e14c5e32c12d
inahym-tech <68362849+inahym-tech@users.noreply.github.com>
2020-11-25 03:06:50 +0900

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -2,8 +2,8 @@
 // @name        script
 // @namespace   script
 // @include     https://example.com/
-// @updateURL   https://github.com/inahym-tech/TestScript/
-// @supportURL  https://github.com/inahym-tech/TestScript/
+// @updateURL   https://github.com/inahym-tech/TestScript/raw/main/script.user.js
+// @supportURL  https://github.com/inahym-tech/TestScript/raw/main/script.user.js
 // @version     1.0
 // ==/UserScript==
 
